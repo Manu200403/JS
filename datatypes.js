feefaad4d3fa7696275cef6c8e5f328cd001e941
Manu200403 <manu.m200403@gmail.com>
2026-02-17 23:06:54 +0530

    datatype

---
+++ datatypes.js
@@ -0,0 +1,24 @@
+"use strict" // used by new js engines but nowadays it is not required(newer version)
+
+//alert(3+3)  works only for browser not for  node.js
+
+console.log(3+3) //code readability should be high
+console.log("Manu");
+
+//datatypes
+let name="Ram"//string datatype
+let age=18//int datatype
+let islog=false//boolean datatype
+
+/*1)NUmber->BigData
+2)string
+3)boolean
+4)null->return  object(empty value)
+5)undefined*/
+
+console.log(typeof age)
+console.log(typeof name);
+console.log(typeof null);
+console.log(typeof undefined);
+
+
