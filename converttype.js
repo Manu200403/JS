6d2bed401d619ac799cc38e701158528342e6383
Manu200403 <manu.m200403@gmail.com>
2026-02-18 22:58:05 +0530

    conversionoftype

---
+++ converttype.js
@@ -0,0 +1,28 @@
+let score="33abc"
+console.log(typeof score);
+console.log(typeof(score));
+
+
+let valueinNUmber=Number(score) //converting type to integer from sring
+console.log(valueinNUmber);
+console.log(typeof valueinNUmber);
+
+/*"33"=>33 string converts to number
+"33abc"=>NaN
+true=>1
+false=>0*/
+
+let islog=1
+let bolleanlogin=Boolean(islog);
+console.log(islog);
+
+console.log(typeof islog);
+console.log(typeof bolleanlogin);
+console.log( bolleanlogin);
+
+
+/*true=1 false=0
+""=>false
+"Manu"=>true
+*/
+
