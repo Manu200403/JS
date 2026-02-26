93beae51b1c5e07a4e54c4bd55ba360455e48adf
Manu200403 <manu.m200403@gmail.com>
2026-02-26 23:05:25 +0530

    arrays method

--- arrays.js
+++ arrays.js
@@ -1,4 +1,4 @@
-let arr=[1,2,"MANU",true]//elements can be of any datatypes
+ let arr=[1,2,"MANU",true]//elements can be of any datatypes
    console.log(arr[0]);
    
    const newarr=new Array(1,2,3,100,200,300)
@@ -29,5 +29,33 @@ let arr=[1,2,"MANU",true]//elements can be of any datatypes
     console.log(newarr);
     
     
+   const marvel_heros=["Thor","Hulk","Ironman","spiderman"]
+    const dc_heros=["Batman","Flash","Superman"]
+
+    marvel_heros.push(dc_heros)//push functions output u get is [ 'Thor', 'Hulk', 'Ironman', [ 'Batman', 'Flash', 'Superman' ] ]
+   console.log(marvel_heros);
    
+
+console.log(marvel_heros.concat(dc_heros));//[ 'Thor', 'Hulk', 'Ironman', [ 'Batman', 'Flash', 'Superman' ] ] same as push func
+
+const new_arr=[...marvel_heros,...dc_heros]//['Thor','Hulk','Ironman','spiderman','Batman','Flash','Superman']
+console.log(new_arr);
+
+const arrnew=[1,2,3,4,5,[6,7,8],[9,10,[11,12]]]
+console.log(arrnew);
+const realarr=arrnew.flat(Infinity)//[1,  2, 3, 4,  5,6,  7, 8, 9, 10, 11, 12]
+console.log(realarr);
+
+console.log(Array.isArray("Manu"));//rerurn false as Manu is string not array
+console.log(Array.from("Manu"));//converts string to array
+console.log(Array.from({name:"Manu"}));//u get [] empty array as u should mention either to convert key or value to convert to array
+
+let s1=100
+let s2=200
+let s3=300
+console.log(Array.of(s1,s2,s3));//to form array
+
+
+
+
    
