9689e1b304fa1e38bb11b6726257134be9fd0c17
Manu200403 <manu.m200403@gmail.com>
2026-03-02 22:49:04 +0530

    object_destructure_api

--- objects.js
+++ objects.js
@@ -1,4 +1,5 @@
-
+//objects can be either literals or singleton
+//singleton-Has only one instance (cant be created more than one time)
 
 const mysymbol=Symbol("Key1")
 
@@ -37,11 +38,81 @@ jsuser.greetings=function(){
 
 jsuser.greetings2=function()
 {
-    console.log(`Hello js user ${this.name}`);
+    console.log(`Hello js user ${this.name}`);//this is use to refer reference of same object
     
 }
 console.log(jsuser.greetings());
 console.log(jsuser.greetings2());
 
 
+//const tinderuser=new Object();//singletone objects
+ const tinderuser={}//other way of declaring array
+//output of tinderuser will{} as it is empty
 
+ tinderuser.id='123bca'
+ tinderuser.name='Manu'
+ tinderuser.isloggedin=false
+
+ console.log(tinderuser);
+
+ //objects inside objects
+
+ const regularuser={
+    email:"ram@gmail.com",
+    fullname:{
+        username:"Manu",
+        lastname:"M"
+    }
+ }
+ 
+console.log(regularuser.fullname);
+
+//combining objects
+const obj1={1:"a",2:"b"}
+const obj2={3:"a",4:"b"}
+
+//const obj3={obj1,obj2}
+//console.log(obj3);//{ obj1: { '1': 'a', '2': 'b' }, obj2: { '3': 'a', '4': 'b' } } object inside object it is not combined
+
+//use of assign() to combine
+obj3=Object.assign({},obj1,obj2)//{}->targer obj1 and obj2 is inside target and returned 
+console.log(obj3);//comined { '1': 'a', '2': 'b', '3': 'a', '4':'b' }
+
+//use spread operaror
+obj4={...obj1,...obj2}
+console.log((obj4));
+
+const user=[
+    {
+        id:1,
+        email:"nam@google.com "
+    },
+    {
+        id:2,
+        email:"nbdihs@g,ail.com"
+    }
+]
+
+console.log(user[1].email)
+
+console.log(Object.keys(tinderuser));//get keys  of object tinderuser
+
+console.log(Object.values(tinderuser));//get values  of object tinderus er
+console.log(Object.entries(tinderuser));//ckey is converted to array and val also
+
+console.log(tinderuser.hasOwnProperty('isLoggedin'));//chk wheather property exists or not
+
+
+
+//destructure of objects-lets to extract values of objecst and store it in varaible which later can be used
+const course={
+    course:'JS',
+    Student:'Manu'
+}
+
+const{course:subject}=course
+console.log(subject);//output=JS
+
+//api is in format of JSON
+//JSON-Similar to objects cantains keys and values but no name like objects 
+//api can also be in form of arrays
